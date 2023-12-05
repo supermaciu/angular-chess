b49ae3803672e30a0f7d6a766822b63c7d02fa25
supermaciu <m.jankowski2605@gmail.com>
2023-12-06 00:22:05 +0100

    implemented check

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -5,7 +5,7 @@ import { Move } from '../move';
 
 import * as bootstrap from 'bootstrap';
 
-declare var window: any;
+declare var window: any; // for displaying bootstrap modals
 
 @Component({
   selector: 'app-board',
@@ -69,12 +69,32 @@ export class BoardComponent implements OnInit {
   // -repetition - The threefold-repetition rule says that if a position arises three times in a game, either player can claim a draw during that position
   // -agreement from both players
 
-  //TODO: CHECKMATE !!!!!!!!!!!!!!!!!!
+  // TO MAKE IT WORK
+  //TODO: check
+  //TODO: cant make moves that endanger your king -> delete those moves from legalMoves for every piece
+  //TODO: when in check you need to remove it using the king or other pieces or by capturing the attacking enemy piece
+  //TODO: checkmate
+  //TODO: draw
+  //TODO: castling can be only done if king is not in check
+  //TODO: add danger highlight for king
+
+  // LATER TODOS
+  //TODO: count pieces' values to see who's ahead
+  //TODO: dynamic evaluation
+  //TODO: animation
+  //TODO: sounds
+  //TODO: optimisations
+  //TODO: more encapsulation / dividing things
+  //TODO: bootstrap Modal compontent for displaying stuff
+  //TODO: divide functions like noDrag, getMouseCoordinates to its own services
+  //TODO: make everything more structural
+  //TODO: simplify Board component's template to feature one function for every element that handles all events
 
   promotionModal: any;
 
   ngOnInit() {
     this.resetBoard();
+
     this.promotionModal = new window.bootstrap.Modal(document.getElementById("promotionModal"));
   }
 
@@ -123,6 +143,182 @@ export class BoardComponent implements OnInit {
     this.mouseTop = event.clientY - 50;
   }
 
+  evaluateLegalMoves(tile: Tile) {
+    if (tile.piece === undefined)
+      return;
+
+    let legalMoves: Tile[] = [];
+    let piece = tile.piece;
+
+    switch (piece.type) {
+      case PIECETYPES.PAWN: {
+        let delta = (piece.color == 0b01000) ? -1 : 1; // depends on color
+        if (tile.y+delta >= 0 && tile.y+delta <= 7 && this.getTile(tile.x, tile.y+delta).piece === undefined) legalMoves.push(this.getTile(tile.x, tile.y+delta));
+        if (tile.y+2*delta >= 0 && tile.y+2*delta <= 7 && !piece.touched && this.getTile(tile.x, tile.y+delta).piece === undefined) legalMoves.push(this.getTile(tile.x, tile.y+2*delta));
+
+        // capturing
+        if (tile.x-1 >= 0 && tile.y+delta >= 0 && tile.y+delta <= 7 && this.getTile(tile.x-1, tile.y+delta).piece !== undefined) {
+          legalMoves.push(this.getTile(tile.x-1, tile.y+delta));
+        }
+        if (tile.x+1 <= 7 && tile.y+delta >= 0 && tile.y+delta <= 7 && this.getTile(tile.x+1, tile.y+delta).piece !== undefined) {
+          legalMoves.push(this.getTile(tile.x+1, tile.y+delta));
+        }
+
+        // en passant
+        if (tile.x-1 >= 0 && this.getTile(tile.x-1, tile.y+delta).piece === undefined && this.getTile(tile.x-1, tile.y).piece !== undefined && this.getTile(tile.x-1, tile.y).piece!.color != piece.color && this.getTile(tile.x-1, tile.y).piece!.enpassantable == true) {
+          legalMoves.push(this.getTile(tile.x-1, tile.y+delta));
+        } 
+        if (tile.x+1 <= 7 && this.getTile(tile.x+1, tile.y+delta).piece === undefined && this.getTile(tile.x+1, tile.y).piece !== undefined && this.getTile(tile.x+1, tile.y).piece!.color != piece.color && this.getTile(tile.x+1, tile.y).piece!.enpassantable == true) {
+          legalMoves.push(this.getTile(tile.x+1, tile.y+delta));
+        }
+        break;
+      }
+
+      case PIECETYPES.KNIGHT: {
+        if (tile.x-1 >= 0 && tile.y+2 >= 0 && tile.x-1 <= 7 && tile.y+2 <= 7) legalMoves.push(this.getTile(tile.x-1, tile.y+2));
+        if (tile.x+1 >= 0 && tile.y+2 >= 0 && tile.x+1 <= 7 && tile.y+2 <= 7) legalMoves.push(this.getTile(tile.x+1, tile.y+2));
+
+        if (tile.x+2 >= 0 && tile.y+1 >= 0 && tile.x+2 <= 7 && tile.y+1 <= 7) legalMoves.push(this.getTile(tile.x+2, tile.y+1));
+        if (tile.x+2 >= 0 && tile.y-1 >= 0 && tile.x+2 <= 7 && tile.y-1 <= 7) legalMoves.push(this.getTile(tile.x+2, tile.y-1));
+
+        if (tile.x-1 >= 0 && tile.y-2 >= 0 && tile.x-1 <= 7 && tile.y-2 <= 7) legalMoves.push(this.getTile(tile.x-1, tile.y-2));
+        if (tile.x+1 >= 0 && tile.y-2 >= 0 && tile.x+1 <= 7 && tile.y-2 <= 7) legalMoves.push(this.getTile(tile.x+1, tile.y-2));
+
+        if (tile.x-2 >= 0 && tile.y+1 >= 0 && tile.x-2 <= 7 && tile.y+1 <= 7) legalMoves.push(this.getTile(tile.x-2, tile.y+1));
+        if (tile.x-2 >= 0 && tile.y-1 >= 0 && tile.x-2 <= 7 && tile.y-1 <= 7) legalMoves.push(this.getTile(tile.x-2, tile.y-1));
+        break;
+      }
+
+      case PIECETYPES.BISHOP: {
+        for (let i = 1; i <= Math.min(tile.x, tile.y); i++) {
+          legalMoves.push(this.getTile(tile.x-i, tile.y-i));
+          if (this.getTile(tile.x-i, tile.y-i).piece !== undefined) break;
+        }
+        for (let i = 1; i <= Math.min(7 - tile.x, tile.y); i++) {
+          legalMoves.push(this.getTile(tile.x+i, tile.y-i));
+          if (this.getTile(tile.x+i, tile.y-i).piece !== undefined) break;
+        }
+        for (let i = 1; i <= Math.min(7 - tile.x, 7 - tile.y); i++) {
+          legalMoves.push(this.getTile(tile.x+i, tile.y+i));
+          if (this.getTile(tile.x+i, tile.y+i).piece !== undefined) break;
+        }
+        for (let i = 1; i <= Math.min(tile.x, 7 - tile.y); i++) {
+          legalMoves.push(this.getTile(tile.x-i, tile.y+i));
+          if (this.getTile(tile.x-i, tile.y+i).piece !== undefined) break;
+        }
+        break;
+      }
+
+      case PIECETYPES.ROOK: {
+        for (let i = 1; i <= tile.y; i++) {
+          legalMoves.push(this.getTile(tile.x, tile.y-i));
+          if (this.getTile(tile.x, tile.y-i).piece !== undefined) break;
+        }
+        for (let i = 1; i <= 7 - tile.x; i++) {
+          legalMoves.push(this.getTile(tile.x+i, tile.y));
+          if (this.getTile(tile.x+i, tile.y).piece !== undefined) break;
+        }
+        for (let i = 1; i <= 7 - tile.y; i++) {
+          legalMoves.push(this.getTile(tile.x, tile.y+i));
+          if (this.getTile(tile.x, tile.y+i).piece !== undefined) break;
+        }
+        for (let i = 1; i <= tile.x; i++) {
+          legalMoves.push(this.getTile(tile.x-i, tile.y));
+          if (this.getTile(tile.x-i, tile.y).piece !== undefined) break;
+        }
+        break;
+      }
+
+      case PIECETYPES.QUEEN: {
+        for (let i = 1; i <= Math.min(tile.x, tile.y); i++) {
+          legalMoves.push(this.getTile(tile.x-i, tile.y-i));
+          if (this.getTile(tile.x-i, tile.y-i).piece !== undefined) break;
+        }
+        for (let i = 1; i <= Math.min(7 - tile.x, tile.y); i++) {
+          legalMoves.push(this.getTile(tile.x+i, tile.y-i));
+          if (this.getTile(tile.x+i, tile.y-i).piece !== undefined) break;
+        }
+        for (let i = 1; i <= Math.min(7 - tile.x, 7 - tile.y); i++) {
+          legalMoves.push(this.getTile(tile.x+i, tile.y+i));
+          if (this.getTile(tile.x+i, tile.y+i).piece !== undefined) break;
+        }
+        for (let i = 1; i <= Math.min(tile.x, 7 - tile.y); i++) {
+          legalMoves.push(this.getTile(tile.x-i, tile.y+i));
+          if (this.getTile(tile.x-i, tile.y+i).piece !== undefined) break;
+        }
+
+        for (let i = 1; i <= tile.y; i++) {
+          legalMoves.push(this.getTile(tile.x, tile.y-i));
+          if (this.getTile(tile.x, tile.y-i).piece !== undefined) break;
+        }
+        for (let i = 1; i <= 7 - tile.x; i++) {
+          legalMoves.push(this.getTile(tile.x+i, tile.y));
+          if (this.getTile(tile.x+i, tile.y).piece !== undefined) break;
+        }
+        for (let i = 1; i <= 7 - tile.y; i++) {
+          legalMoves.push(this.getTile(tile.x, tile.y+i));
+          if (this.getTile(tile.x, tile.y+i).piece !== undefined) break;
+        }
+        for (let i = 1; i <= tile.x; i++) {
+          legalMoves.push(this.getTile(tile.x-i, tile.y));
+          if (this.getTile(tile.x-i, tile.y).piece !== undefined) break;
+        }
+        break;
+      }
+
+      case PIECETYPES.KING: {
+        if (tile.y-1 >= 0) legalMoves.push(this.getTile(tile.x, tile.y-1));
+        if (tile.x+1 <= 7) legalMoves.push(this.getTile(tile.x+1, tile.y));
+        if (tile.y+1 <= 7) legalMoves.push(this.getTile(tile.x, tile.y+1));
+        if (tile.x-1 >= 0) legalMoves.push(this.getTile(tile.x-1, tile.y));
+
+        if (tile.x-1 >= 0 && tile.y-1 >= 0) legalMoves.push(this.getTile(tile.x-1, tile.y-1));
+        if (tile.x-1 >= 0 && tile.y+1 <= 7) legalMoves.push(this.getTile(tile.x-1, tile.y+1));
+        if (tile.x+1 <= 7 && tile.y+1 <= 7) legalMoves.push(this.getTile(tile.x+1, tile.y+1));
+        if (tile.x+1 <= 7 && tile.y-1 >= 0) legalMoves.push(this.getTile(tile.x+1, tile.y-1));
+        
+        // TODO: castling: king cant be checked
+        // - Neither the king nor the rook has previously moved. done
+        // - There are no pieces between the king and the rook. done
+        // - The king is not currently in check.
+        // - The king does not pass through or finish on a square that is attacked by an enemy piece.
+
+        // long castling / queenside
+        if (piece.touched) {
+          let rookTile = this.getTile(0, tile.y);
+
+          let tileInBetween1 = this.getTile(1, tile.y);
+          let tileInBetween2 = this.getTile(2, tile.y);
+          let tileInBetween3 = this.getTile(3, tile.y);
+
+          if (rookTile.piece !== undefined && !rookTile.piece.touched && tileInBetween1.piece === undefined && tileInBetween2.piece === undefined && tileInBetween3.piece === undefined) {
+            legalMoves.push(rookTile);
+          }
+        }
+
+        // short castling / kingside
+        if (piece.touched) {
+          let rookTile = this.getTile(7, tile.y);
+
+          let tileInBetween1 = this.getTile(5, tile.y);
+          let tileInBetween2 = this.getTile(6, tile.y);
+
+          if (rookTile.piece !== undefined && !rookTile.piece.touched && tileInBetween1.piece === undefined && tileInBetween2.piece === undefined) {
+            legalMoves.push(rookTile);
+          }
+        }
+      }
+    }
+
+    legalMoves = legalMoves.filter((t) => {
+      return t.piece === undefined || t.piece.color !== piece.color || t.piece.castlingable;
+    });
+
+    legalMoves.push(tile);
+
+    return legalMoves;
+  }
+
   selectPiece(clickedTile: Tile) {
     if (this.selectedPiece === undefined && clickedTile.piece !== undefined && clickedTile.piece.color === this.playerTurnColor) {
       this.selectedPiece = clickedTile.piece;
@@ -136,14 +332,13 @@ export class BoardComponent implements OnInit {
       this.previousTile = clickedTile;
       this.selectedPieceMove.from = clickedTile.coordinate;
 
-      // legal, no collision checking moves
-      // TODO: move this to Tile class, because evaluation is being done every time piece is selected, make a Board class to be able to import it anywhere
       switch (this.selectedPiece.type) {
         case PIECETYPES.PAWN: {
           let delta = (this.selectedPiece!.color == 0b01000) ? -1 : 1; // depends on color
           if (clickedTile.y+delta >= 0 && clickedTile.y+delta <= 7 && this.getTile(clickedTile.x, clickedTile.y+delta).piece === undefined) this.selectedPieceLegalTiles.push(this.getTile(clickedTile.x, clickedTile.y+delta));
           if (clickedTile.y+2*delta >= 0 && clickedTile.y+2*delta <= 7 && !this.selectedPiece!.touched && this.getTile(clickedTile.x, clickedTile.y+delta).piece === undefined) this.selectedPieceLegalTiles.push(this.getTile(clickedTile.x, clickedTile.y+2*delta));
 
+          // capturing
           if (clickedTile.x-1 >= 0 && clickedTile.y+delta >= 0 && clickedTile.y+delta <= 7 && this.getTile(clickedTile.x-1, clickedTile.y+delta).piece !== undefined) {
             this.selectedPieceLegalTiles.push(this.getTile(clickedTile.x-1, clickedTile.y+delta));
           }
@@ -164,9 +359,6 @@ export class BoardComponent implements OnInit {
               this.getTile(clickedTile.x+1, clickedTile.y).erasePiece();
             }
           }
-
-          // TODO: promotion
-          
           break;
         }
 
@@ -310,7 +502,7 @@ export class BoardComponent implements OnInit {
         return tile.piece === undefined || tile.piece.color !== this.playerTurnColor || tile.piece.castlingable;
       });
 
-      this.selectedPieceLegalTiles.push(clickedTile); // selectedPiece's origin
+      this.selectedPieceLegalTiles.push(clickedTile); // piece's origin
 
       for (let tile of this.selectedPieceLegalTiles) {
         tile.availableMove = true;
@@ -347,22 +539,33 @@ export class BoardComponent implements OnInit {
 
           if (clickedTile.y == 0 || clickedTile.y == 7) {
             this.promotionModal.show();
-            
-            
           }
         }
 
-        this.selectedPiece.touched = true;
+        if (this.selectedPiece.touched === false)
+          this.selectedPiece.touched = true;
+
         this.moves.push({...this.selectedPieceMove}); // pushing copy of Move object
 
         this.playerTurnColor = (this.playerTurnColor == PIECECOLORS.WHITE) ? PIECECOLORS.BLACK : PIECECOLORS.WHITE;
+
+        this.previousClickedTile = clickedTile;
+
+        // check, testing after changing the player turn
+        let kingTile = this.findTileById(this.playerTurnColor ^ PIECETYPES.KING);
+        let kingAttackers = this.findKingAttackers(kingTile!); // there always should be a king on the board
+
+        // king in check
+        if (kingAttackers !== undefined && kingAttackers.length > 0) {
+          console.log(PIECECOLORS[kingTile!.piece!.color] + " king in check");
+
+          //checkmate...
+        }
 
         console.log(`${PIECECOLORS[(this.selectedPiece.color)]} ${PIECETYPES[this.selectedPiece.type]} ${this.selectedPieceMove.from} -> ${this.selectedPieceMove.to}`);
       }
 
       this.selectedPiece = undefined;
-
-      this.previousClickedTile = clickedTile;
     }
   }
 
@@ -412,5 +615,171 @@ export class BoardComponent implements OnInit {
 
       tile.setPiece(new Piece(old_color, new_type));
     }
+  }
+
+  findTileById(id: number) {
+    for (let row of this.board) {
+      for (let tile of row) {
+        if (tile.piece !== undefined && tile.piece.id === id)
+          return tile;
+      }
+    }
+
+    return;
+  }
+
+  findKingAttackers(kingTile: Tile) {
+
+    let kingPiece = kingTile.piece;
+
+    if (kingPiece === undefined)
+      return;
+
+    let x = kingTile.x;
+    let y = kingTile.y;
+
+    // get potencial attackers 
+    let potencialAttackers: Tile[] = [];
+
+    for (let i = 1; i <= Math.min(x, y); i++) {
+      let tile = this.getTile(x-i, y-i);
+      if (tile.piece !== undefined) {
+        if (tile.piece.color !== kingPiece.color)
+          potencialAttackers.push(tile);
+      
+        break;
+      }
+    }
+    for (let i = 1; i <= Math.min(7 - x, y); i++) {
+      let tile = this.getTile(x+i, y-i);
+      if (tile.piece !== undefined) {
+        if (tile.piece.color !== kingPiece.color)
+          potencialAttackers.push(tile);
+
+        break;
+      }
+    }
+    for (let i = 1; i <= Math.min(7 - x, 7 - y); i++) {
+      let tile = this.getTile(x+i, y+i);
+      if (tile.piece !== undefined) {
+        if (tile.piece.color !== kingPiece.color)
+          potencialAttackers.push(tile);
+
+        break;
+      }
+    }
+    for (let i = 1; i <= Math.min(x, 7 - y); i++) {
+      let tile = this.getTile(x-i, y+i);
+      if (tile.piece !== undefined) {
+        if (tile.piece.color !== kingPiece.color)
+          potencialAttackers.push(tile);
+
+        break;
+      }
+    }
+
+    for (let i = 1; i <= y; i++) {
+      let tile = this.getTile(x, y-i);
+      if (tile.piece !== undefined) {
+        if (tile.piece.color !== kingPiece.color)
+          potencialAttackers.push(tile);
+        
+        break;
+      }
+    }
+    for (let i = 1; i <= 7 - x; i++) {
+      let tile = this.getTile(x+i, y);
+      if (tile.piece !== undefined) {
+        if (tile.piece.color !== kingPiece.color)
+          potencialAttackers.push(tile);
+        
+        break;
+      }
+    }
+    for (let i = 1; i <= 7 - y; i++) {
+      let tile = this.getTile(x, y+i);
+      if (tile.piece !== undefined) {
+        if (tile.piece.color !== kingPiece.color)
+          potencialAttackers.push(tile);
+        
+        break;
+      }
+    }
+    for (let i = 1; i <= x; i++) {
+      let tile = this.getTile(x-i, y);
+      if (tile.piece !== undefined) {
+        if (tile.piece.color !== kingPiece.color)
+          potencialAttackers.push(tile);
+        
+        break;
+      }
+    }
+
+    if (x-1 >= 0 && y+2 >= 0 && x-1 <= 7 && y+2 <= 7) {
+      let knightTile1 = this.getTile(x-1, y+2);
+      if (knightTile1.piece !== undefined && knightTile1.piece.color !== kingPiece.color)
+        potencialAttackers.push(knightTile1);
+    }
+    
+    if (x+1 >= 0 && y+2 >= 0 && x+1 <= 7 && y+2 <= 7) {
+      let knightTile2 = this.getTile(x+1, y+2);
+      if (knightTile2.piece !== undefined && knightTile2.piece.color !== kingPiece.color)
+        potencialAttackers.push(knightTile2);
+    }
+      
+
+    if (x+2 >= 0 && y+1 >= 0 && x+2 <= 7 && y+1 <= 7) {
+      let knightTile3 = this.getTile(x+2, y+1);
+      if (knightTile3.piece !== undefined && knightTile3.piece.color !== kingPiece.color)
+        potencialAttackers.push(knightTile3);
+    }
+      
+    if (x+2 >= 0 && y-1 >= 0 && x+2 <= 7 && y-1 <= 7) {
+      let knightTile4 = this.getTile(x+2, y-1);
+      if (knightTile4.piece !== undefined && knightTile4.piece.color !== kingPiece.color)
+        potencialAttackers.push(knightTile4);
+    }
+      
+
+    if (x-1 >= 0 && y-2 >= 0 && x-1 <= 7 && y-2 <= 7) {
+      let knightTile5 = this.getTile(x-1, y-2);
+      if (knightTile5.piece !== undefined && knightTile5.piece.color !== kingPiece.color)
+        potencialAttackers.push(knightTile5);
+    }
+      
+    if (x+1 >= 0 && y-2 >= 0 && x+1 <= 7 && y-2 <= 7) {
+      let knightTile6 = this.getTile(x+1, y-2);
+      if (knightTile6.piece !== undefined && knightTile6.piece.color !== kingPiece.color)
+        potencialAttackers.push(knightTile6);
+    }
+      
+
+    if (x-2 >= 0 && y+1 >= 0 && x-2 <= 7 && y+1 <= 7) {
+      let knightTile7 = this.getTile(x-2, y+1);
+      if (knightTile7.piece !== undefined && knightTile7.piece.color !== kingPiece.color)
+        potencialAttackers.push(knightTile7);
+    }
+      
+    if (x-2 >= 0 && y-1 >= 0 && x-2 <= 7 && y-1 <= 7) {
+      let knightTile8 = this.getTile(x-2, y-1);
+      if (knightTile8.piece !== undefined && knightTile8.piece.color !== kingPiece.color)
+        potencialAttackers.push(knightTile8);
+    }
+      
+
+    // king attackers found by checking if potencialAttacker's legal moves contain this king's tile 
+    let attackers: Tile[] = [];
+
+    for (let potencialAttacker of potencialAttackers) {
+      let potencialAttackerLegalMoves = this.evaluateLegalMoves(potencialAttacker);
+
+      if (potencialAttackerLegalMoves === undefined)
+        continue;
+
+      if (potencialAttackerLegalMoves.includes(kingTile))
+        attackers.push(potencialAttacker);
+    }
+
+    return attackers;
   }
 }
